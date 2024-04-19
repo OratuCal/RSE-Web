e08eef56a50c5b98d621357a4f7f66be4d9c0a86
otarucal <otarucal@yahoo.com>
2024-04-19 22:58:37 +1000

    create and add the footer component

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -45,7 +45,7 @@ export default function SignUp() {
           <Link 
             to="/" 
             className="font-bold
-            dark:text-white" text-4xl>
+            dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500
             via-purple-500 to-pink-400 rounded-lg text-white">Jocelyn</span>
             Blog
