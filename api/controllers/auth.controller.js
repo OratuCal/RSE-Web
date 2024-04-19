e3b0ea4c6203fab5d153a05f59e11482074a9294
otarucal <otarucal@yahoo.com>
2024-04-20 00:00:33 +1000

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     // console.log(req.body);
@@ -21,6 +22,30 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.json('Signup Successful');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            next(errorHandler(404, 'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid Password'));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true}).json(validUser);
     } catch (error) {
         next(error);
     }
